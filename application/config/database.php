39cfeab8027c2d185c49d8c00836b5629cf8fa57
Nur Muhammad <blog.nurmuhammad@gmail.com>
2018-12-15 14:27:26 +0700

    Add .gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
-	'password' => 'password',
+	'password' => 'tanpapassword',
 	'database' => 'tubes',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
