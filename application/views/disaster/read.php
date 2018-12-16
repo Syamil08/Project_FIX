046730d9e7fd442ee8ad0fbdad612fb334a9da79
Nasution <nasutionscience@gmail.com>
2018-12-16 14:36:21 +0700

    template

--- application/views/disaster/read.php
+++ application/views/disaster/read.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Detail Donasi</title>
+	<title>Donasi</title>
 	
 	<!-- My Fonts -->
 	<!-- My Fonts -->
@@ -16,54 +16,46 @@
 	<!-- Akhir My Css -->
 
 </head>
-<body>		<!-- Navbar -->
-	
-	<nav class="navbar navbar-expand-lg navbar-light">
+<body>	
+		<?php 
+			$this->load->view('template/nav');
+		 ?>
+
+		<br><br>
+
+
+		
 		<div class="container">
-		  <a class="navbar-brand" href="#">DONASIKU</a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-		    <div class="navbar-nav ml-auto">
-		      <a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
-		      <a class="nav-item nav-link" href="#">Donasi</a>
-		      <a class="nav-item nav-link" href="<?php echo site_url('disaster/about')?>">About</a>
-			<?php if (userdata()): ?>
-				<li class="nav-item dropdown no-arrow">
-		          <a href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-roggle">
-		            <i class="fa fa-user-circle fa-fw"></i>
-		          </a>
-		          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-		            <a href="#" class="dropdown-item"><?php echo userdata()->username?></a>
-		            <a href="<?php echo site_url('donatur/index')?>" class="dropdown-item">Dashboard</a>
-		            <div class="dropdown-divider"></div>
-		            <a href="<?php echo site_url('user/logout')?>" class="dropdown-item">Logout</a>
-		          </div>
-		        </li>
-		        <?php else: ?>
-				<a href="<?php echo site_url('user/index')?>" class="nav-item btn btn-primary tombol" href="#">Join Us</a>
-			<?php endif ?>
-		    </div>
-		  </div>
-		</div>
-	</nav>
+			<h2><b>Rumah roboh, akibat Gunung Meletus</b></h2><br>
+			<dic class="row">
+				<div class="col-8" style="background-color: ">
+					<div class="card-img-top img-responsive" style="width: 100%;">
+						  <img src="<?php echo base_url()?>assets/images/gunung.jpg" alt="" class="img-responsive"><br><br>
+						  <h5><b>Rumah roboh, akibat Gunung Meletus</b></h5><br>
 
-	<!-- Akhir Navbar -->
-<?php 
+						  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis, repellendus reprehenderit a ex labore nemo sunt mollitia quibusdam cupiditate atque officiis tempora, consectetur eligendi quia, molestias aspernatur quidem iste? Odit, inventore. Placeat sint, sit nesciunt deserunt! Fugit assumenda, minima velit voluptatibus est. Ut quasi, a iste suscipit perspiciatis quas!</p>
+			</div>
+				</div>
+
+				<div class="col-4" style="background-color: ">
+					<h3 class="text-center"><b>Bantuan Terkumpul</b></h3>
 					
-		foreach ($asal as $value) {
-			 echo $value->title;
-			 echo $value->description; 
+					<div class="col btn-warning text-center" style="padding:2%; background-color: #F1F3F4;" ><b>455 </b> Barang</div>
 
-		}
-		?>
-		
-		
-		
-		
+					<br>
+					<?php echo anchor(site_url('donasi/doDonasi'),'Mulai Donasi', 'class="btn btn-primary"'); ?>
 
-		<script src="<?php echo base_url()?>assets/js/jquery-3.3.1.min.js"></script>
-		<script src="<?php echo base_url()?>assets/libs/bootstrap/js/bootstrap.min.js"></script>	
+
+				</div>
+			</dic>
+		</div>
+			<br><br>
+		
+		<?php $this->load->view('template/footer'); ?>
+		
+			<script src="<?php echo base_url()?>assets/js/jquery-3.3.1.min.js"></script>
+			<script src="<?php echo base_url()?>assets/libs/bootstrap/js/bootstrap.min.js"></script>	
+	
+
 </body>
 </html>
